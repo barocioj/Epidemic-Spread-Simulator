4267967e412be4ca364cf447b265afcec2b9ae58
Jesus Barocio <77757573+barocioj@users.noreply.github.com>
2023-11-27 13:51:59 -0800

    Create simulator.py

---
+++ simulator.py
@@ -0,0 +1,29 @@
+<div class="project">
+    <h3>Epidemic Spread Simulation</h3>
+    <p>
+        This Python program uses the NetworkX and Matplotlib libraries to simulate the spread of an epidemic within a randomly generated network. The simulation considers various parameters, including infection rate, recovery rate, incubation rate, and vaccination probability. The interactive sliders allow users to control the simulation parameters and observe the progression of the epidemic over a specified number of days.
+    </p>
+
+    <h4>Simulation Parameters:</h4>
+    <ul>
+        <li><strong>Nodes:</strong> Number of nodes in the network.</li>
+        <li><strong>Edges:</strong> Number of edges in the network.</li>
+        <li><strong>Infection Rate:</strong> Probability of infection when a susceptible node interacts with an infected node.</li>
+        <li><strong>Recovery Rate:</strong> Probability of recovery for an infected node.</li>
+        <li><strong>Incubation Rate:</strong> Probability of transitioning from exposed to infected.</li>
+        <li><strong>Vaccination Probability:</strong> Probability of vaccination for susceptible nodes.</li>
+        <li><strong>Days:</strong> Number of days for the simulation.</li>
+    </ul>
+
+    <p>
+        The program visualizes the epidemic spread, highlighting susceptible (blue), infected (red), recovered (green), exposed (purple), and vaccinated (yellow) nodes. Each day, the simulation updates the network state based on the specified parameters.
+    </p>
+
+    <h4>How to Use:</h4>
+    <ol>
+        <li>Adjust the sliders to set the initial parameters.</li>
+        <li>Click the "Run Simulation" button to start the interactive epidemic spread simulation.</li>
+    </ol>
+
+    <p>GitHub Repository: <a href="your-github-repo-link-for-epidemic-simulation" target="_blank">Epidemic Spread Simulation</a></p>
+</div>
